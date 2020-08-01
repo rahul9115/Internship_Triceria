ba271bca35e0363e52588c68fcb156e758344e20
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-08-01 21:07:41 +0530

    Update random.js

--- project/src/js/random.js
+++ project/src/js/random.js
@@ -6,7 +6,7 @@ import blue from "../components/blue"
 import something from "./intialize";
 var m;
 function createMarkup() {
-    createMarkup.counter = localStorage.getItem("rahul1234");
+    createMarkup.counter = localStorage.getItem("rahul123456");
     console.log(createMarkup.counter);
 
     if (!createMarkup.counter) {
@@ -19,16 +19,16 @@ function createMarkup() {
     console.log(createMarkup.counter);
     if (createMarkup.counter == 1) {
         m = 0;
-        localStorage.setItem("rahul1234", createMarkup.counter);
+        localStorage.setItem("rahul123456", createMarkup.counter);
     } else {
-        var obj = JSON.parse(localStorage.getItem('numb1234'));
+        var obj = JSON.parse(localStorage.getItem('numb123456'));
         m = obj.m;
     }
     m = m + 1;
     var b = { "m": m };
 
     var json = JSON.stringify(b);
-    localStorage.setItem("numb1234", json);
+    localStorage.setItem("numb123456", json);
     let l = m * 100;
 
     var a = Math.floor(Math.random() * l);
