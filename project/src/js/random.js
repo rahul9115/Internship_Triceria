d8b2b66e4329b3019e9046f5c1cd20a5884b846a
unknown <sudharahul2010@gmail.com>
2020-08-01 20:18:56 +0530

    Complete

--- project/src/js/random.js
+++ project/src/js/random.js
@@ -6,32 +6,29 @@ import blue from "../components/blue"
 import something from "./intialize";
 var m;
 function createMarkup() {
-    createMarkup.counter = localStorage.getItem("counter1");
+    createMarkup.counter = localStorage.getItem("rahul1234");
     console.log(createMarkup.counter);
-    if (typeof createMarkup.counter == 'undefined') {
 
-        createMarkup.counter = 0;
-
-    }
     if (!createMarkup.counter) {
         createMarkup.counter = 0;
+
     }
-    console.log(createMarkup.counter);
+
     createMarkup.counter++;
 
-
+    console.log(createMarkup.counter);
     if (createMarkup.counter == 1) {
         m = 0;
-        console.log(m);
+        localStorage.setItem("rahul1234", createMarkup.counter);
     } else {
-        var obj = JSON.parse(localStorage.getItem('number3'));
+        var obj = JSON.parse(localStorage.getItem('numb1234'));
         m = obj.m;
     }
     m = m + 1;
     var b = { "m": m };
-    console.log(b.m)
+
     var json = JSON.stringify(b);
-    localStorage.setItem("number3", json);
+    localStorage.setItem("numb1234", json);
     let l = m * 100;
 
     var a = Math.floor(Math.random() * l);
