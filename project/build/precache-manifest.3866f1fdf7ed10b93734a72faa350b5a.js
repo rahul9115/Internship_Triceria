8e2d9ebf85cc6935964bcf37cd3d44e7d18d5d53
unknown <sudharahul2010@gmail.com>
2020-08-01 12:51:34 +0530

    Push heroku

---
+++ project/build/precache-manifest.3866f1fdf7ed10b93734a72faa350b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a49120efa9d0beddb7fa",
+    "url": "/static/css/main.93a40357.chunk.css"
+  },
+  {
+    "revision": "a49120efa9d0beddb7fa",
+    "url": "/static/js/main.a98febd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c58b9c672ebf133be2c8",
+    "url": "/static/js/2.1db5e550.chunk.js"
+  },
+  {
+    "revision": "92272ad0ec141c6fdd351e87f0256ee5",
+    "url": "/index.html"
+  }
+];
